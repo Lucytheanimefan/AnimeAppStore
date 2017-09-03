3b928e18b9b041eb9c9e2762b9756c2a2e83ee92
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-09-02 20:52:27 -0400

    Started anime app store project

--- AnimeMacAppStore/ViewController.swift
+++ AnimeMacAppStore/ViewController.swift
@@ -9,16 +9,30 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    @IBOutlet weak var updateButton: NSButton!
+    @IBOutlet weak var animeButton: NSButton!
+    var buttons: [NSButton]!
+    
+    var selectedIndex: Int!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        selectedIndex = 0
+        buttons = [updateButton, animeButton]
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
+        }
+    }
+    
+    
+    @IBAction func didPressTab(_ sender: NSButton) {
+        if (sender.identifier == "anime")
+        {
+            // Update anime view
         }
     }
 
