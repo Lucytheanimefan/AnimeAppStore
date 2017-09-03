fe3900fcc34effbe6724a3c190452c99204c72d8
Lucy Zhang <lucyzhang@lucytheanimefan-2.local>
2017-09-02 20:32:41 -0400

    Initial Commit

---
+++ AnimeMacAppStore/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AnimeMacAppStore
+//
+//  Created by Lucy Zhang on 9/2/17.
+//  Copyright © 2017 Lucy Zhang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
